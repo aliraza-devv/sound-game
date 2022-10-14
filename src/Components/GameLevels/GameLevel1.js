f6c859160c6458c087e5e53927f8da0224423ff7
Ali Raza <aliraza.dev.web@gmail.com>
2022-10-14 17:29:59 +0500

    design game level 1

--- src/Components/GameLevels/GameLevel1.js
+++ src/Components/GameLevels/GameLevel1.js
@@ -1,7 +1,30 @@
+import one from '../../images/one.png';
+import two from '../../images/two.png';
+import three from '../../images/three.png';
+import four from '../../images/four.png';
+import five from '../../images/five.png';
+
 const GameLevel1 = () => {
     return (
-        <div>
-            Game Level 1
+        <div className="main">
+            <div className="container">
+                <div className="icons">
+                    <img src={five} alt='One' width={50} height={50} />
+                    <img src={four} alt='One' width={50} height={50} />
+                    <img src={two} alt='One' width={50} height={50} />
+                    <img src={one} alt='One' width={50} height={50} />
+                    <img src={three} alt='One' width={50} height={50} />
+                </div>
+                <div className="numbers">
+                    <ul className="style-lists">
+                        <li className='list'>2</li>
+                        <li className='list'>1</li>
+                        <li className='list'>4</li>
+                        <li className='list'>3</li>
+                        <li className='list'>5</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
